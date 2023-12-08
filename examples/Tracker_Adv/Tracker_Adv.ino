8a40ac8bc025e3e24bd8f830d28ab38ef83a5054
SeeedWenzy <zhiyi.wen@seeed.cc>
2023-12-08 15:44:55 +0800

    1.style:add comment

--- examples/Tracker_Adv/Tracker_Adv.ino
+++ examples/Tracker_Adv/Tracker_Adv.ino
@@ -5,15 +5,18 @@
 
 void setup()
 {
+    // Initializes the debug output
     Serial.begin(115200);
-    while (!Serial) delay(100);  
+    while (!Serial) delay(100);     // Wait for ready  
 
     Serial.print("WM1110 BLE Advertising Example\r\n");  
 
-    wm1110_ble.begin();
+    wm1110_ble.begin(); // Init BLE
 
-    wm1110_ble.setName("Wio Tracker 1110");
+    wm1110_ble.setName("Wio Tracker 1110"); // Set the  Bluetooth broadcast name "Wio Tracker 1110"
 
+    // Set broadcast parameters
+    // true: central,  false:peripheral
     wm1110_ble.setStartParameters(false);
 
 }
@@ -21,9 +24,15 @@ void setup()
 void loop()
 {    
     delay(1000);
+
+    // Start broadcast
     printf("Starting adv---------\r\n");
     wm1110_ble.startAdv();
+
+   // The duration of the broadcast is 10s
     delay(10000);
+    
+    // Stop broadcast
     wm1110_ble.stopAdv();
     printf("Stoped adv---------\r\n");
 
--- examples/Transmit_Receive/Transmit_Receive.ino
+++ examples/Transmit_Receive/Transmit_Receive.ino
@@ -3,32 +3,46 @@
 #include <WM1110_Geolocation.hpp>
 #include <WM1110_BLE.hpp>
 
-uint8_t ble_scan_results_buf[244] = {0};
-uint8_t ble_scan_results_size = 0;
+// Ble receives data buffer
+uint8_t ble_rec_buf[244] = {0};
+
+// Ble receives data size
+uint8_t ble_rec_size = 0;
+
 void setup()
 {
+    // Initializes the debug output
     Serial.begin(115200);
-    while (!Serial) delay(100);  
+    while (!Serial) delay(100);     // Wait for ready  
 
     Serial.print("WM1110 BLE Transmit & Receive Example\r\n");  
 
-    wm1110_ble.begin();
+    wm1110_ble.begin(); // Init BLE
 
-    wm1110_ble.setName("Wio Tracker 1110");
+    wm1110_ble.setName("Wio Tracker 1110"); // Set the  Bluetooth broadcast name "Wio Tracker 1110"
 
+    // Set broadcast parameters
+    // true: central,  false:peripheral   empty:both 
     wm1110_ble.setStartParameters();
 
+    // Start broadcast
     wm1110_ble.startAdv();
 }
 
 void loop()
 {    
-    if(wm1110_ble.getBleRecData(ble_scan_results_buf,&ble_scan_results_size))
+    // Get the data received by Bluetooth
+    if(wm1110_ble.getBleRecData(ble_rec_buf,&ble_rec_size)) 
     {
-        printf("%s",ble_scan_results_buf);
-        wm1110_ble.ble_trace_print("%s",ble_scan_results_buf);
-        memset(ble_scan_results_buf,0,ble_scan_results_size);
-        ble_scan_results_size = 0;
+        // Print the data received by Bluetooth
+        printf("%s",ble_rec_buf);
+
+        // The received data is circuited back
+        wm1110_ble.ble_trace_print("%s",ble_rec_buf);
+
+        // Clear buffer & size
+        memset(ble_rec_buf,0,ble_rec_size);
+        ble_rec_size = 0;
     }
 }
 
